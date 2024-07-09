ac6c4bac155b0f19f7a890dedeaf17e661d04eb4
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-09 22:15:14 +0900

    [Silver IV] Title: 스택 2, Time: 148 ms, Memory: 6300 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+stack<int> S;
+int N, order, x;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> order;
+        if (order == 1) {
+            cin >> x;
+            S.push(x);
+        }
+        else if (order == 2) {
+            if (!S.empty()) {
+                cout << S.top() << '\n'; S.pop();
+            }
+            else cout << -1 << '\n';
+        }
+        else if (order == 3) {
+            cout << S.size() << '\n';
+        }
+        else if (order == 4) {
+            if (S.empty()) cout << 1 << '\n';
+            else cout << 0 << '\n';
+        }
+        else {
+            if (!S.empty()) cout << S.top() << '\n';
+            else cout << -1 << '\n';
+        }
+    }
+    return 0;
+}
