3cfb0f25a751c53c11eabc6c8f1881fdc8f420bf
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-07-12 14:42:19 +0900

    Update 안전 영역.cc
    DFS 풀이

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -54,3 +54,48 @@ int main() {
 	cout << cnt;
 	return 0;
 }
+
+// DFS 풀이
+#include <bits/stdc++.h>
+using namespace std;
+int N, board[100][100], ret, cnt;
+bool visited[100][100];
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+
+void DFS(int x, int y, int h) {
+    for (int dir = 0; dir < 4; dir++) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+        if (visited[ny][nx]) continue;
+        if (board[ny][nx] <= h) continue;
+        visited[ny][nx] = true;
+        DFS(nx, ny, h);
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> board[i][j];
+        }
+    }
+    for (int h = 0; h <= 100; h++) {
+        cnt = 0;
+        memset(visited, false, sizeof(visited));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] > h && !visited[i][j]) {
+                    DFS(j, i, h);
+                    cnt++;
+                }
+            }
+        }
+        ret = max(ret, cnt);
+    }
+    cout << ret;
+    return 0;
+}
