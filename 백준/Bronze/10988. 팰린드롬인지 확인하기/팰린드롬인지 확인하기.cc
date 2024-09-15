0533093db709c1893e9faff418139317ad5d9e2a
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-09-15 13:10:03 +0900

    Update 팰린드롬인지 확인하기.cc

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -11,3 +11,18 @@ int main() {
     else cout << 0;
     return 0;
 }
+
+// 다른 풀이 (reverse)
+#include <bits/stdc++.h>
+using namespace std;
+string word, temp;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> word;
+    temp = word;
+    reverse(temp.begin(), temp.end());
+    if (temp == word) cout << 1;
+    else cout << 0;
+    return 0;
+}
