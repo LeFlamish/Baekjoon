a6b327313f24e68fe8ea77dabd826960e7343dad
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-09-26 01:05:27 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int A, B;
+	while (1) {
+		scanf("%d %d", &A, &B);
+		if (A == 0 && B == 0) break;
+		printf("%d\n", A + B);
+	}
+	return 0;
+}
