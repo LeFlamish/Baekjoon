90dbc875ff393da5143cfa7d0bf6ea5ac8975e7f
정지욱 <164985246+LeFlamish@users.noreply.github.com>
2024-09-26 00:18:04 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -1,19 +1,17 @@
-#include <iostream>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
 int main() {
 	int score;
-	cin >> score;
-	if (score >= 90) cout << 'A';
+	scanf("%d", &score);
+	if (score >= 90) putchar('A');
 	else {
-		if (score >= 80) cout << 'B';
+		if (score >= 80) putchar('B');
 		else {
-			if (score >= 70) cout << 'C';
+			if (score >= 70) putchar('C');
 			else {
-				if (score >= 60) cout << 'D';
-				else {
-					cout << 'F';
-				}
+				if (score >= 60) putchar('D');
+				else putchar('F');
 			}
 		}
 	}
